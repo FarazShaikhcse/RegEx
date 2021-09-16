96eb38a4a48d587f51b53f40b87d0868e8ae5cef
Faraz Shaikh <farazshabbir@113AMBBLR-Faraz-Shabbir-Shaikh.local>
2021-09-16 23:30:32 +0530

    uc2 last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,17 +4,27 @@ public class UserRegistration {
 
 	public static void main(String[] args) {
 
-		final String first_name_pt = "^[A-Z][a-z]{2,}";
+		final String first_name_pt = "^[A-Z][a-zA-Z]{2,}";
+		final String last_name_pt = "^[A-Z][a-zA-Z]{2,}";
 
 		String firstName = "Faraz";
+		String lastName = "Shaikh";
 
 		if (Pattern.matches(first_name_pt, firstName)) {
-			
+
 			System.out.println("First name is valid");
-		} 
-		else {
-			
+		} else {
+
 			System.out.println("First name is invalid");
+		}
+
+		if (Pattern.matches(last_name_pt, lastName)) {
+
+			System.out.println("Last name is valid");
+
+		} else {
+
+			System.out.println("Last name is invalid");
 		}
 
 	}
