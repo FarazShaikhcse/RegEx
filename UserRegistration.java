1d93caa91c5b7d289b59f3a6cefb4a34b70f84a2
Faraz Shaikh <farazshabbir@113AMBBLR-Faraz-Shabbir-Shaikh.local>
2021-09-21 21:33:53 +0530

    exception handling

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,47 +2,26 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-	public static void main(String[] args) {
+	final static String first_name_pt = "^[A-Z][a-zA-Z]{2,}";
+	final static String last_name_pt = "^[A-Z][a-zA-Z]{2,}";
 
-		final String first_name_pt = "^[A-Z][a-zA-Z]{2,}";
-		final String last_name_pt = "^[A-Z][a-zA-Z]{2,}";
+	final static String emailpt = "^[a-zA-Z0-9-_+]+(\\.?[a-zA-Z0-9-_]+)@[a-zA-Z0-9-_]+\\.[a-zA-Z]{2,}(\\.?[a-zA-Z-_]+)";
 
-		final String emailpt = "^[a-zA-Z0-9-_+]+(\\.?[a-zA-Z0-9-_]+)@[a-zA-Z0-9-_]+\\.[a-zA-Z]{2,}(\\.?[a-zA-Z-_]+)";
+	final static String mob = "[1-9][0-9]\\s[1-9][0-9]{9}";
 
-		final String mob = "[1-9][0-9]\\s[1-9][0-9]{9}";
+	final static String passwordpt = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}$";
 
-		final String passwordpt = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}$";
 
-		String firstName = "Faraz";
-		String lastName = "Shaikh";
-		String email[] = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.con", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com",
-				"abc@gmail.com.com", "abc+100@gmail.com" };
-		String mobno = "91 9876543210";
-		String password = "hypeSqu@d11";
 
-		System.out.print("First name: ");
-		validate(first_name_pt, firstName);
-		System.out.print("Last name: ");
-		validate(last_name_pt, lastName);
-		for (String string : email) {
-
-			System.out.print("Email: ");
-			validate(emailpt, string);
-		}
-		System.out.print("Mobile no: ");
-		validate(mob, mobno);
-		System.out.print("Password: ");
-		validate(passwordpt, password);
-
-	}
-
-	private static void validate(String pattern, String string) {
+	static boolean validate(String pattern, String string) throws UserRegException {
 
 		if (Pattern.matches(pattern, string)) {
 
 			System.out.println(string + " is valid");
+			return true;
 		} else {
-			System.out.println(string + " is invalid");
+			
+			throw new UserRegException(string + " is invalid");
 		}
 
 	}
